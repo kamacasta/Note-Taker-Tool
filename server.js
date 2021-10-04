6d1ce3e657af5789b7d0a39978ce6261a4483565
kama casta <kamacasta983@gmail.com>
2021-10-04 01:14:55 -0700

    created server.js,  added express working on Middleware logic at the moment

---
+++ server.js
@@ -0,0 +1,12 @@
+// require express
+const express = require('express');
+
+// require path
+const path = require('path');
+
+// express call
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Middleware for the incoming data to be parsed
