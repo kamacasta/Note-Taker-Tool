45eb7beb840faac626a2af307204934da0eba848
kama casta <kamacasta983@gmail.com>
2021-10-05 01:45:19 -0700

    logic to the .get

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,20 @@
 const router = require('express').Router();
 // to interact with database 
 const fs = require("fs");
-const path = require('path');
 
-router.get('', (req, res) => {
 
+router.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', "utf-8", (err, data) => {
+        const notes = JSON.parse(data);
+        res.send(notes);
+        if (err) {
+            throw err;
+        }
+    })
 })
 
-router.post('', (req, res) => {
-    
+router.post('/api/notes', (req, res) => {
+
 })
 
 router.delete('', (req, res) => {
