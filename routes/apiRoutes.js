2a0e76203c2b64f1175d815a7d0d26f5586ff330
kama casta <kamacasta983@gmail.com>
2021-10-05 17:15:44 -0700

    added to notes router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,37 @@
 // to create to router object
 const router = require('express').Router();
+const { randomUUID } = require('crypto');
 // to interact with database 
 const fs = require("fs");
 
-
+// Router for getting the notes from db.json
 router.get('/api/notes', (req, res) => {
+    // fs module used to correspond to db also used on line 24, and 40
     fs.readFile('./db/db.json', "utf-8", (err, data) => {
-        const notes = JSON.parse(data);
-        res.send(notes);
+        // data being parsed from db.json also used on line 25, and 41
+        const noteData = JSON.parse(data);
+        res.send(noteData);
+        // if theres errors it will be caught and displayed in the console
         if (err) {
             throw err;
-        }
-    })
-})
+        };
+    });
+});
 
+// Router to post notes from the user, and also saves the notes created
 router.post('/api/notes', (req, res) => {
+    req.body.id = UUID.v1();
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        const noteData = JSON.parse(data);
+        noteData.push(req.body);
+        fs.writeFile('./db/db.json', JSON.stringify(noteData), (err) => {
+            res.json(req.body);
+             // if theres errors it will be caught and displayed in the console
+            if (err) {
+                throw err;
+            }
+        });
+    });
+});
 
-})
-
-router.delete('', (req, res) => {
-    
-})
 
